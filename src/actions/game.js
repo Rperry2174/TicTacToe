b1fc6e890ee47d3c0f2987ec0d231e4a0ea467e4
Ryan <rperry2174@gmail.com>
2019-11-13 10:27:54 -0800

    Start using linter

--- src/actions/game.js
+++ src/actions/game.js
@@ -9,142 +9,144 @@ import {
   EDIT_ROOM_CODE,
   ADD_PLAYER,
   ASSIGN_NETWORK_ID,
-  SYNC_PLAYERS
+  SYNC_PLAYERS,
 } from '../constants';
 
 export function changeBoard(board) {
   return {
     type: BOARD_CHANGE,
-    payload: board
-  }
+    payload: board,
+  };
 }
 
 export function changeGameMode(mode) {
   return {
     type: GAME_MODE_CHANGE,
-    payload: mode
-  }
+    payload: mode,
+  };
 }
 
 export function restartGame(mode) {
   return {
     type: RESTART_GAME,
-    payload: mode
-  }
+    payload: mode,
+  };
 }
 
 export function changeGameState(gameState) {
   return {
     type: GAME_STATE_CHANGE,
-    payload: gameState
-  }
+    payload: gameState,
+  };
 }
 
 export function editPlayer(playerIndex, playerName) {
   return {
     type: EDIT_PLAYER,
-    payload: { playerIndex: playerIndex, playerName: playerName }
-  }
+    payload: {
+      playerIndex,
+      playerName,
+    },
+  };
 }
 
 export function addPlayer() {
   return {
     type: ADD_PLAYER,
-    payload: ""
-  }
+    payload: '',
+  };
 }
 
 export function syncPlayers(players) {
   return {
     type: SYNC_PLAYERS,
-    payload: players
-  }
+    payload: players,
+  };
 }
 
 export function assignNetworkId(networkId) {
   return {
     type: ASSIGN_NETWORK_ID,
-    payload: networkId
-  }
+    payload: networkId,
+  };
 }
 
 export function editRoomCode(roomCode) {
   return {
     type: EDIT_ROOM_CODE,
-    payload: roomCode
-  }
+    payload: roomCode,
+  };
 }
 
 export function newTurn(playerIndex) {
   // Toggle back and forth between 0 and 1
-  let newPlayerIndex = playerIndex == 0 ? 1 : 0;
+  const newPlayerIndex = playerIndex === 0 ? 1 : 0;
   return {
     type: NEW_TURN,
-    payload: newPlayerIndex
-  }
+    payload: newPlayerIndex,
+  };
 }
 
 export function updateWinningPlayer(matrix) {
-
   // TODO: Find a cleaner way to do this
   // For now, if player 1 (index 0) wins then the sum of their squares will be 0
   //          if player 2 (index 1) wins the nthe sum of their squares will be 3
-  const winningSums = [0, 3]
+  const winningSums = [0, 3];
 
-  let rowSums = {
+  const rowSums = {
     0: 0,
     1: 0,
-    2: 0
-  }
+    2: 0,
+  };
 
-  let columnSums = {
+  const columnSums = {
     0: 0,
     1: 0,
-    2: 0
-  }
+    2: 0,
+  };
 
-  let diagonalSums = {
+  const diagonalSums = {
     topLeftToBottomRight: 0,
-    topRightToBottomLeft: 0
-  }
+    topRightToBottomLeft: 0,
+  };
 
-  for(let row = 0; row < 3; row++){
+  for (let row = 0; row < 3; row++) {
     //check full row
-    for(let col = 0; col < 3; col++){
-      let currentVal = matrix[row][col]
-      columnSums[col] += currentVal
-      rowSums[row] += currentVal
+    for (let col = 0; col < 3; col++) {
+      const currentVal = matrix[row][col];
+      columnSums[col] += currentVal;
+      rowSums[row] += currentVal;
 
-      if(row == col) {
+      if (row === col) {
         diagonalSums.topLeftToBottomRight += currentVal;
       }
 
-      if (row + col == 2) {
+      if (row + col === 2) {
         diagonalSums.topRightToBottomLeft += currentVal;
       }
     }
   }
 
-  let winningPlayerValues = [];
-  [rowSums, columnSums, diagonalSums].forEach((direction) => {
-    console.log("direction: ", direction);
-    Object.values(direction).forEach((values) => {
-      winningPlayerValues.push(values)
-    })
-  })
+  const winningPlayerValues = [];
+  [rowSums, columnSums, diagonalSums].forEach(direction => {
+    console.log('direction: ', direction);
+    Object.values(direction).forEach(values => {
+      winningPlayerValues.push(values);
+    });
+  });
 
-  let winningPlayerIndex = null
-  for(let i = 0; i < winningPlayerValues.length; i++) {
-    winningPlayerIndex = winningSums.indexOf(winningPlayerValues[i])
-    if(winningPlayerIndex >= 0) {
+  let winningPlayerIndex = null;
+  for (let i = 0; i < winningPlayerValues.length; i++) {
+    winningPlayerIndex = winningSums.indexOf(winningPlayerValues[i]);
+    if (winningPlayerIndex >= 0) {
       break;
     } else {
-      winningPlayerIndex = null
+      winningPlayerIndex = null;
     }
   }
 
   return {
     type: UPDATE_WINNING_PLAYER,
-    payload: winningPlayerIndex
-  }
+    payload: winningPlayerIndex,
+  };
 }
