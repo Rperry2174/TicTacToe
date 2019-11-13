36de7b554503deeee30bfa61abbbd814014aa34e
Ryan <rperry2174@gmail.com>
2019-11-13 12:19:28 -0800

    Lint the title page... NOTE could get rid of it completely

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,98 +1,20 @@
-import React, { Component } from 'react'
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ImageBackground,
-} from 'react-native'
+import React, {Component} from 'react';
+import {View} from 'react-native';
 
-import GameModeSelection from './GameModeSelection'
-import NetworkInput from './NetworkInput'
-import TwoPlayerInput from './TwoPlayerInput'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-import { connect } from 'react-redux';
-import { } from '../actions/game';
-import { bindActionCreators } from 'redux';
+import GameModeSelection from './GameModeSelection';
 
 class Title extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomCode: '1234',
-      playerName: 'Ryan'
-    };
-  }
-
-  onChangeText = (key, text) => {
-
-  }
-
-  playGameButtonPress = () => {
-    this.props.actions.changeGameState("game")
-  }
-
   render() {
     return (
       <View>
-        <GameModeSelection></GameModeSelection>
-        <Text
-          style={styles.inputLabel}>
-        </Text>
+        <GameModeSelection />
       </View>
-    )
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  vertical: {
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    padding: 10,
-    height:'100%'
-  },
-  inputBox: {
-     height: 40,
-     borderColor: 'gray',
-     borderWidth: 1
-  },
-  inputLabel: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#000000',
-  },
-  mainTitle: {
-    fontSize: 48,
-    fontWeight: '600',
-    color: '#000000',
-    textAlign: 'center',
-    marginTop: 30
-  },
-  backgroundImage: {
-    flex: 1,
-    flexDirection: 'column',
-    resizeMode: 'stretch',
-    width:'100%',
-    height:'100%',
-  },
-  positioningContainer: {
-    flex: 1,
-    // backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  chalkBoardContainer: {
-    width:'100%',
-    height: '70%',
-  },
-  playGameContainer: {
-    height: '15%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-})
 
 const mapStateToProps = state => ({
   ...state,
@@ -102,4 +24,7 @@ const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({}, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Title)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Title);
