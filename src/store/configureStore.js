3069856a8f60eefc307f60696e1252f33a9fbee3
Ryan <rperry2174@gmail.com>
2019-11-13 12:45:42 -0800

    lint configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers} from 'redux';
 import gameReducer from '../reducers/gameReducer';
 
-const rootReducer = combineReducers(
-  {
-    game: gameReducer
-  }
-);
+const rootReducer = combineReducers({
+  game: gameReducer,
+});
 
 const configureStore = () => {
   return createStore(rootReducer);
-}
+};
 
 export default configureStore;
