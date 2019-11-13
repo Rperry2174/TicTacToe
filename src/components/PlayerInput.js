7e90b3f958b18aec257e9966923a048f27b24281
Ryan <rperry2174@gmail.com>
2019-11-13 11:27:59 -0800

    Playerinput linting

--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,53 +1,44 @@
-import React, { Component } from 'react'
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Image,
-  ImageBackground,
-} from 'react-native'
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-import GameModeSelection from './GameModeSelection'
-import NetworkInput from './NetworkInput'
-import TwoPlayerInput from './TwoPlayerInput'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-import { GAME_MODE_OPTIONS } from '../constants'
+import NetworkInput from './NetworkInput';
+import TwoPlayerInput from './TwoPlayerInput';
 
-import { connect } from 'react-redux';
-import { changeGameState } from '../actions/game';
-import { bindActionCreators } from 'redux';
+import {GAME_MODE_OPTIONS} from '../constants';
+import {changeGameState} from '../actions/game';
 
 class PlayerInput extends Component {
-
   constructor(props) {
     super(props);
+
+    this.playGameButtonPress = this.playGameButtonPress.bind(this);
   }
 
-  playGameButtonPress = () => {
-    this.props.actions.changeGameState("game")
+  playGameButtonPress() {
+    this.props.actions.changeGameState('game');
   }
 
   render() {
     return (
       <View>
-        <Text
-          style={styles.gameModeText}>
-          { GAME_MODE_OPTIONS[this.props.game.mode] }
+        <Text style={styles.gameModeText}>
+          {GAME_MODE_OPTIONS[this.props.game.mode]}
         </Text>
-        { this.props.game.mode == 1 && <TwoPlayerInput/> }
-        { this.props.game.mode == 2 && <NetworkInput/> }
+        {this.props.game.mode === 1 && <TwoPlayerInput />}
+        {this.props.game.mode === 2 && <NetworkInput />}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   inputBox: {
-     height: 40,
-     borderColor: 'gray',
-     borderWidth: 1
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 40,
   },
   inputLabel: {
     fontSize: 24,
@@ -55,45 +46,45 @@ const styles = StyleSheet.create({
     color: '#000000',
   },
   gameModeText: {
+    color: '#e6e6e6',
+    fontFamily: 'squeakychalksound',
     fontSize: 45,
     textAlign: 'center',
-    fontFamily: "squeakychalksound",
-    color: '#e6e6e6'
   },
   vertical: {
     flexDirection: 'column',
-    height:'100%',
+    height: '100%',
   },
   mainTitle: {
+    color: '#000000',
     fontSize: 48,
     fontWeight: '600',
-    color: '#000000',
+    marginTop: 30,
     textAlign: 'center',
-    marginTop: 30
   },
   positioningContainer: {
-    flex: 1,
+    alignItems: 'center',
     // backgroundColor: 'blue',
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
   },
   playGameContainer: {
+    alignItems: 'center',
     height: '15%',
     justifyContent: 'center',
-    alignItems: 'center'
   },
   chalkBoardContainer: {
-    width:'100%',
+    width: '100%',
     height: '70%',
   },
   backgroundImage: {
     flex: 1,
     flexDirection: 'column',
     resizeMode: 'stretch',
-    width:'100%',
-    height:'100%',
+    width: '100%',
+    height: '100%',
   },
-})
+});
 
 const mapStateToProps = state => ({
   ...state,
@@ -103,4 +94,7 @@ const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({changeGameState}, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PlayerInput)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PlayerInput);
