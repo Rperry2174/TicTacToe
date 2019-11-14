eae7c39be1eae8eae076822dc79713a45ad9ee1f
Ryan <rperry2174@gmail.com>
2019-11-13 17:49:41 -0800

    cleanup App,js entry point

--- App.js
+++ App.js
@@ -1,132 +1,47 @@
-import React, { Component } from 'react'
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  ImageBackground,
-} from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, View, StatusBar} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import Game from './src/components/screens/Game';
 import GameModeSelection from './src/components/screens/GameModeSelection';
-import GameOver from './src/components/screens/GameOver';
 import PlayerInput from './src/components/screens/PlayerInput';
 
 import Chalkboard from './src/components/background/Chalkboard';
 import WoodPanel from './src/components/background/WoodPanel';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <>
+      <View>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
           <View style={styles.sectionContainer}>
             <View style={styles.vertical}>
               <Chalkboard>
-                {this.props.game.gameState == "gameModeSelection" && <GameModeSelection /> }
-                {this.props.game.gameState == "playerInput" && <PlayerInput /> }
-                {this.props.game.gameState == "game" && <Game /> }
+                {this.props.game.gameState === "gameModeSelection" && <GameModeSelection /> }
+                {this.props.game.gameState === "playerInput" && <PlayerInput /> }
+                {this.props.game.gameState === "game" && <Game /> }
               </Chalkboard>
-              <WoodPanel>
-                <Text> asdfasdfasf </Text>
-              </WoodPanel>
+              <WoodPanel />
             </View>
           </View>
         </SafeAreaView>
-      </>
+      </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
   sectionContainer: {
     marginTop: 32,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
   vertical: {
     flexDirection: 'column',
-    height:'100%',
-  },
-  mainTitle: {
-    fontSize: 48,
-    fontWeight: '600',
-    color: '#000000',
-    textAlign: 'center',
-    marginTop: 30
-  },
-  positioningContainer: {
-    flex: 1,
-    // backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  playGameContainer: {
-    height: '15%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  chalkBoardContainer: {
-    width:'100%',
-    height: '70%',
-  },
-  backgroundImage: {
-    flex: 1,
-    flexDirection: 'column',
-    resizeMode: 'stretch',
-    width:'100%',
-    height:'100%',
+    height: '100%',
   },
 });
-
 
 const mapStateToProps = state => ({
   ...state,
@@ -136,4 +51,7 @@ const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({}, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
